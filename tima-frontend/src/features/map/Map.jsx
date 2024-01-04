761e17e75fff3375b090fe3d7cef1e5de14d7580
dk-chevalier <contact@chevalierrecords.com>
2024-01-04 18:03:12 +0800

    added markers to map, but need fixing

--- tima-frontend/src/features/map/Map.jsx
+++ tima-frontend/src/features/map/Map.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
 import 'mapbox-gl/dist/mapbox-gl.css';
 import '../../index.css';
 import { useGeolocation } from '../../hooks/useGeolocation';
+import { useVenues } from '../venues/useVenues';
 
 // TODO: Make this a environment variable
 const MAP_TOKEN =
@@ -13,6 +14,9 @@ mapboxgl.accessToken = MAP_TOKEN;
 function Map() {
   const mapContainerRef = useRef(null);
   const map = useRef(null);
+  const { isLoading: isLoadingVenues, venues } = useVenues();
+
+  // console.log(venues);
 
   const [mapPosition, setMapPosition] = useState(null);
   const [zoom, setZoom] = useState(12);
@@ -54,16 +58,24 @@ function Map() {
       // add map onload event (used to resize the map to its container and able to add functionality to logitad data later)
       map.current.on('load', () => {
         map.current.resize();
+
+        // FIXME: CURRENTLY MARKERS ARE SLIGHTLY LEFT OF ACTUAL POSITION, AND MOVE WITH ZOOM....
+        if (isLoadingVenues) return;
+        venues.data.map((venue) =>
+          new mapboxgl.Marker({ anchor: 'bottom', color: '#faa8fb' })
+            .setLngLat(venue.location.coordinates)
+            .addTo(map.current),
+        );
       });
 
       // TODO: clean up on unmount (LEARN WHAT THIS IS ACTUALLY DOING)
       return () => map.current.remove();
     },
-    [mapPosition, zoom],
+    [mapPosition, zoom, isLoadingVenues, venues],
   );
 
   return isLoadingPosition ? (
-    <p>Loading...</p>
+    <p>Loading Data...</p>
   ) : (
     <div className="map-container" ref={mapContainerRef} />
   );
