33bc38049ff44cdcf575a09631f417e47f78e5a6
dk-chevalier <contact@chevalierrecords.com>
2024-01-06 18:08:57 +0800

    got markers working, still slightly wrong place, and color not working yet

--- tima-frontend/src/features/map/Map.jsx
+++ tima-frontend/src/features/map/Map.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
-import '../../index.css';
+import Map, { Source, Layer } from 'react-map-gl';
 import { useGeolocation } from '../../hooks/useGeolocation';
 import { useVenues } from '../venues/useVenues';
 import { HiMiniMapPin } from 'react-icons/hi2';
@@ -10,16 +9,12 @@ import { HiMiniMapPin } from 'react-icons/hi2';
 const MAP_TOKEN =
   'pk.eyJ1IjoiZGstY2hldmFsaWVyIiwiYSI6ImNscXJ0bHF1dDJoc24yanJ5NnVnZ2xxZXAifQ.0rhupFXU4AuDPu-PMhg-cw';
 
-mapboxgl.accessToken = MAP_TOKEN;
-
-function Map() {
-  const mapContainerRef = useRef(null);
-  const map = useRef(null);
+function MapContainer() {
   const { isLoading: isLoadingVenues, venues } = useVenues();
+  const mapRef = useRef(null);
 
-  // console.log(venues);
-
-  const [mapPosition, setMapPosition] = useState(null);
+  const [mapLng, setMapLng] = useState(null);
+  const [mapLat, setMapLat] = useState(null);
   const [zoom, setZoom] = useState(12);
 
   const {
@@ -30,96 +25,77 @@ function Map() {
 
   useEffect(
     function () {
-      if (!geolocationPosition) getPosition();
+      if (!geolocationPosition) return getPosition();
 
-      setMapPosition(geolocationPosition);
-      console.log(geolocationPosition);
+      const [lng, lat] = geolocationPosition;
+
+      setMapLng(lng);
+      setMapLat(lat);
+      // console.log(lng, lat);
     },
     [geolocationPosition, getPosition],
   );
 
-  // Creating our map...
-  useEffect(
-    function () {
-      if (map.current) return;
+  console.log(mapLng);
+  console.log(mapLat);
 
-      // FIXME: Check if this is a bad practice??? (i.e. waiting to load map until mapPosition has users coordinates).....PROBABLY BAD BECAUSE WON'T LOAD MAP IF CAN'T GET USERS COORDS
-      if (!mapPosition) return;
+  if (isLoadingVenues) return;
+  const geojsonMarkers = {
+    type: 'FeatureCollection',
+    features: venues.data.map((venue) => {
+      const data = {
+        type: 'Feature',
+        geometry: venue.location,
+        properties: {
+          title: 'Mapbox',
+        },
+      };
+      return data;
+    }),
+  };
+  console.log(geojsonMarkers);
 
-      map.current = new mapboxgl.Map({
-        container: mapContainerRef.current,
-        style: 'mapbox://styles/dk-chevalier/clqvxp6s5011c01qrakfpaklj',
-        center: mapPosition,
-        zoom: zoom,
-      });
+  // const onLoad = (e) => {
+  //   if (mapRef.current) {
+  //     const pinImage = new Image();
+  //     // pinImage.onload = () => {
+  //     if (!mapRef.current.hasImage('pin')) {
+  //       mapRef.current.addImage('pin', pinImage, { sdf: true });
+  //     }
+  //     // };
+  //     pinImage.src = '../../assets/mapPin.svg';
+  //   }
+  // };
 
-      // add zoom controls to map
-      map.current.addControl(new mapboxgl.NavigationControl(), 'top-right');
-
-      // add map onload event (used to resize the map to its container and able to add functionality to logitad data later)
-      map.current.on('load', () => {
-        map.current.resize();
-
-        // // FIXME: CURRENTLY MARKERS ARE SLIGHTLY LEFT OF ACTUAL POSITION, AND MOVE WITH ZOOM....
-        // if (isLoadingVenues) return;
-        // venues.data.map((venue) =>
-        //   new mapboxgl.Marker({ anchor: 'top', color: '#faa8fb' })
-        //     .setLngLat(venue.location.coordinates)
-        //     .addTo(map.current),
-        // );
-
-        // TODO: Try using layers instead of markers...so need to addSource first
-        if (isLoadingVenues) return;
-        // console.log(venues.data[0].location);
-
-        const geojsonMarkers = {
-          type: 'FeatureCollection',
-          features: venues.data.map((venue) => {
-            const data = {
-              type: 'Feature',
-              geometry: venue.location,
-              properties: {
-                title: 'Mapbox',
-              },
-            };
-            return data;
-          }),
-        };
-        console.log(geojsonMarkers);
-        // const mapIcon =
-
-        for (const feature of geojsonMarkers.features) {
-          new mapboxgl.Marker({ color: '#faa8fb' })
-            .setLngLat(feature.geometry.coordinates)
-            .addTo(map.current);
-        }
-
-        // map.current.addSource('venues', geojsonMarkers);
-
-        // map.current.addLayer({
-        //   id: 'venues-layer',
-        //   type: 'circle',
-        //   source: 'venues',
-        //   paint: {
-        //     'circle-radius': 4,
-        //     'circle-stroke-width': 2,
-        //     'circle-color': 'red',
-        //     'circle-stroke-color': 'white',
-        //   },
-        // });
-      });
-
-      // TODO: clean up on unmount (LEARN WHAT THIS IS ACTUALLY DOING)
-      return () => map.current.remove();
+  const layerStyle = {
+    id: 'point',
+    type: 'symbol',
+    layout: {
+      'icon-image': 'mapPin',
+      'icon-anchor': 'bottom',
     },
-    [mapPosition, zoom, isLoadingVenues, venues],
-  );
+    source: 'my-data',
+  };
 
-  return isLoadingPosition ? (
+  return isLoadingPosition || !mapLng || !mapLat ? (
     <p>Loading Data...</p>
   ) : (
-    <div className="map-container" ref={mapContainerRef} />
+    <Map
+      ref={mapRef}
+      mapboxAccessToken={MAP_TOKEN}
+      initialViewState={{ longitude: mapLng, latitude: mapLat, zoom: zoom }}
+      style={{ width: '100%', height: '100%' }}
+      mapStyle="mapbox://styles/dk-chevalier/clqvxp6s5011c01qrakfpaklj"
+      minPitch={0}
+      maxPitch={0}
+      reuseMaps
+      // onLoad={onLoad}
+    >
+      <Source id="my-data" type="geojson" data={geojsonMarkers}>
+        <Layer {...layerStyle} />
+      </Source>
+    </Map>
   );
 }
 
-export default Map;
+export default MapContainer;
--- tima-frontend/src/pages/MapPage.jsx
+++ tima-frontend/src/pages/MapPage.jsx
@@ -1,12 +1,12 @@
 import Sidebar from '../ui/Sidebar';
-import Map from '../features/map/Map';
+import MapContainer from '../features/map/Map';
 
 function MapPage() {
   return (
     <div className="grid grid-cols-[20vw_1fr] grid-rows-[80vh]">
       <Sidebar />
       <div className="col-span-1 col-start-2" id="mapbox">
-        <Map></Map>
+        <MapContainer></MapContainer>
       </div>
     </div>
   );
