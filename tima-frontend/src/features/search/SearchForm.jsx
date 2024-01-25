5246f5cfe5618fd893564ddff32563a0a014095b
dk-chevalier <contact@chevalierrecords.com>
2024-01-25 18:08:19 +1100

    made possible to toggle what params users want to search by

--- tima-frontend/src/features/search/SearchForm.jsx
+++ tima-frontend/src/features/search/SearchForm.jsx
@@ -1,8 +1,7 @@
 import { HiXMark } from 'react-icons/hi2';
 import Button from '../../ui/Button';
-import { Form, useActionData } from 'react-router-dom';
+import { Form } from 'react-router-dom';
 import { useState } from 'react';
-import { AddressAutofill } from '@mapbox/search-js-react';
 import { useDispatch } from 'react-redux';
 import { closeSearchForm } from './searchFormSlice';
 
@@ -12,12 +11,14 @@ function SearchForm({ onClick }) {
   const [searchFor, setSearchFor] = useState('venues');
   const [searchBy, setSearchBy] = useState('location');
 
+  const [searchByOpen, setSearchByOpen] = useState(false);
+  const [searchByGenres, setSearchByGenres] = useState(false);
+  const [searchByGigType, setSearchByGigType] = useState(false);
+
   const dispatch = useDispatch();
 
-  console.log(searchBy);
-
   return (
-    <div className="w-m absolute left-[2.5%] top-[2.5%] z-50 h-[95%] w-[95%] overflow-y-scroll rounded-md border border-secondary-300 bg-primary-100 px-3 pb-3 pt-5 shadow-lg">
+    <div className="w-m absolute left-[2.5%] top-[2.5%] z-50 h-[95%] w-[95%] overflow-y-scroll rounded-md border border-secondary-300 bg-primary-100 px-3 pb-3 pt-10 text-primary-900 shadow-lg">
       <div className="absolute right-4 top-4">
         <Button type="close" onClick={onClick}>
           <HiXMark />
@@ -32,7 +33,7 @@ function SearchForm({ onClick }) {
           <div className="flex flex-col gap-1">
             <label>What are you searching for?</label>
             <select
-              className="border border-secondary-300 bg-primary-200 shadow-md"
+              className="rounded-sm border border-secondary-300 bg-primary-100 shadow-md"
               name="searchFor"
               required
               defaultValue="venues"
@@ -46,89 +47,130 @@ function SearchForm({ onClick }) {
           </div>
 
           <div className="flex flex-col gap-1">
-            <label>Search by name or location?</label>
-            <select
-              name="searchBy"
-              required
-              onChange={({ target }) => {
-                setSearchBy(target.value);
-              }}
+            <label
+              className={`flex justify-between duration-300 ease-in-out ${
+                searchByOpen ? '' : 'text-gray-400'
+              }`}
             >
-              <option value="location">Search by location</option>
-              <option value="name">Search by name</option>
-            </select>
+              Search by name or location?
+              <input
+                type="checkbox"
+                value=""
+                className="peer appearance-none"
+                onChange={() => setSearchByOpen(!searchByOpen)}
+              />
+              <span className="bg-gray-300 after:bg-gray-100 ml-4 flex h-5 w-9 flex-shrink-0 items-center rounded-full p-1 duration-300 ease-in-out after:h-4 after:w-4 after:rounded-full after:shadow-md after:duration-300 peer-checked:bg-secondary-300 peer-checked:after:translate-x-3"></span>
+            </label>
+            {searchByOpen && (
+              <>
+                <select
+                  className="rounded-sm border border-secondary-300 bg-primary-100 shadow-md"
+                  name="searchBy"
+                  required
+                  onChange={({ target }) => {
+                    setSearchBy(target.value);
+                  }}
+                >
+                  <option value="location">Search by location:</option>
+                  <option value="name">Search by name:</option>
+                </select>
+                {searchBy === 'location' && (
+                  <input
+                    className="rounded-sm border border-secondary-300 bg-primary-100 shadow-md"
+                    type="text"
+                    placeholder="Type in location"
+                    name="searchLocation"
+                    autoComplete="street-address country-name address-level1 address-level2 address-level3 address-level4"
+                  />
+                )}
+                {searchBy === 'name' && (
+                  <input
+                    className="rounded-sm border border-secondary-300 bg-primary-100 shadow-md"
+                    type="text"
+                    placeholder="Type in name"
+                    name="searchName"
+                  ></input>
+                )}
+              </>
+            )}
           </div>
 
           <div className="flex flex-col gap-1">
-            {searchBy === 'location' && (
-              <>
-                <label>Search by location</label>
-                {/* <AddressAutofill
-                  accessToken={MAP_TOKEN}
-                  onSuggest={() => console.log('suggestions')}
-                  onRetrieve={() => console.log('retrieve')}
-                > */}
-                <input
-                  type="text"
-                  placeholder="Search by location"
-                  name="searchLocation"
-                  autoComplete="street-address country-name address-level1 address-level2 address-level3 address-level4"
-                />
-                {/* </AddressAutofill> */}
-              </>
+            <label
+              className={`flex justify-between duration-300 ease-in-out ${
+                searchByGenres ? '' : 'text-gray-400'
+              }`}
+            >
+              Search by genres supported
+              <input
+                type="checkbox"
+                value=""
+                className="peer appearance-none"
+                onChange={() => setSearchByGenres(!searchByGenres)}
+              />
+              <span className="bg-gray-300 after:bg-gray-100 ml-4 flex h-5 w-9 flex-shrink-0 items-center rounded-full p-1 duration-300 ease-in-out after:h-4 after:w-4 after:rounded-full after:shadow-md after:duration-300 peer-checked:bg-secondary-300 peer-checked:after:translate-x-3"></span>
+            </label>
+            {searchByGenres && (
+              <select
+                className="rounded-sm border border-secondary-300 bg-primary-100 shadow-md"
+                defaultValue="saved-genres"
+                name="genres"
+              >
+                <option value="saved-genres">My saved genres</option>
+                <option value="all">All</option>
+                <option value="acoustic">Acoustic</option>
+                <option value="blues">Blues</option>
+                <option value="classical">Classical</option>
+                <option value="country">Country</option>
+                <option value="disco">Disco</option>
+                <option value="electronic">Electronic</option>
+                <option value="folk">Folk</option>
+                <option value="funk">Funk</option>
+                <option value="hip-hop">Hip-hop</option>
+                <option value="indie">Indie</option>
+                <option value="jazz">Jazz</option>
+                <option value="latin">Latin</option>
+                <option value="metal">Metal</option>
+                <option value="pop">Pop</option>
+                <option value="punk">Punk</option>
+                <option value="r&b">R&B</option>
+                <option value="reggae">Reggae</option>
+                <option value="rock">Rock</option>
+                <option value="singer-songwriter">Singer-songrwriter</option>
+                <option value="soul">Soul</option>
+              </select>
             )}
-            {searchBy === 'name' && (
-              <>
-                <label>Search by name</label>
-                <input
-                  type="text"
-                  placeholder="Search by name"
-                  name="searchName"
-                ></input>
-              </>
-            )}
-          </div>
-
-          <div className="flex flex-col gap-1">
-            <label>Search by genres supported</label>
-            <select defaultValue="saved-genres" name="genres">
-              <option value="saved-genres">My saved genres</option>
-              <option value="all">All</option>
-              <option value="acoustic">Acoustic</option>
-              <option value="blues">Blues</option>
-              <option value="classical">Classical</option>
-              <option value="country">Country</option>
-              <option value="disco">Disco</option>
-              <option value="electronic">Electronic</option>
-              <option value="folk">Folk</option>
-              <option value="funk">Funk</option>
-              <option value="hip-hop">Hip-hop</option>
-              <option value="indie">Indie</option>
-              <option value="jazz">Jazz</option>
-              <option value="latin">Latin</option>
-              <option value="metal">Metal</option>
-              <option value="pop">Pop</option>
-              <option value="punk">Punk</option>
-              <option value="r&b">R&B</option>
-              <option value="reggae">Reggae</option>
-              <option value="rock">Rock</option>
-              <option value="singer-songwriter">Singer-songrwriter</option>
-              <option value="soul">Soul</option>
-            </select>
           </div>
 
           {searchFor === 'venues' && (
             <div className="flex flex-col gap-1">
-              <label>Search by types of gigs the venue hosts</label>
-              <select name="gig-type" defaultValue="both">
-                <option value="both">Both</option>
-                <option value="ticketed">Ticketed</option>
-                <option value="free-entry">Free entry</option>
-              </select>
+              <label
+                className={`flex justify-between duration-300 ease-in-out ${
+                  searchByGigType ? '' : 'text-gray-400'
+                }`}
+              >
+                Search by types of gigs the venue hosts
+                <input
+                  type="checkbox"
+                  value=""
+                  className="peer appearance-none"
+                  onChange={() => setSearchByGigType(!searchByGigType)}
+                />
+                <span className="bg-gray-300 after:bg-gray-100 ml-4 flex h-5 w-9 flex-shrink-0 items-center rounded-full p-1 duration-300 ease-in-out after:h-4 after:w-4 after:rounded-full after:shadow-md after:duration-300 peer-checked:bg-secondary-300 peer-checked:after:translate-x-3"></span>
+              </label>
+              {searchByGigType && (
+                <select
+                  className="rounded-sm border border-secondary-300 bg-primary-100 shadow-md"
+                  name="gig-type"
+                  defaultValue="both"
+                >
+                  <option value="both">Both</option>
+                  <option value="ticketed">Ticketed</option>
+                  <option value="free-entry">Free entry</option>
+                </select>
+              )}
             </div>
           )}
-
-          <div className="flex flex-col gap-1"></div>
 
           <Button type="submit">Search</Button>
         </div>
